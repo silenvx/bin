449368338c7982f94689c1b8b44ee99720c360c6
silenvx <czvziwzx@gmail.com>
2014-01-19 04:54:32 +0900

    zipの中身を片っ端からmplayerにやるプログラム書いた

---
+++ z2m.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# zipの中にあるファイルを片っ端からmplayerに渡すプログラム
+
+if [ -f "$1" ];then
+    pipe=`mktemp -u`
+    trap "rm \"${pipe}\";exit 1" 1 2 3 9 15
+
+    mkfifo "${pipe}"||{ echo 'error: mkfifo';exit 1; }
+    IFS=$'\n'
+    for zipInFile in `zipinfo -1 "${1}"|sed -e 's/\[/\\\\[/g' -e 's/\]/\\\\]/g'` ;do
+        echo " --- staring: ${zipInFile}"
+        unzip -p "${1}" "${zipInFile}" > "${pipe}" &
+        mplayer "${pipe}"
+    done
+else
+    echo "not found: $1"
+    exit 1
+fi
