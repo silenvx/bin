16307b82444a78c9231358d93e38cc40b399d801
silenvx <czvziwzx@gmail.com>
2014-03-04 13:18:19 +0900

    修正

--- nico_live.sh
+++ nico_live.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # nico_live.sh rtmp://プロトコル lv[0-9]の放送番号を指定して
-# マイクの音声が流せるだけ
-# 静止画も対応したいがエラーが出て今はよくわからないので保留中
+# マイクの音声と静止画が流せるだけ
+# 実はよくわかってないです
+# こんな名前だけどニコ生以外でも使えると思う、いつか名前を変えるかも
 # エラー処理{{{
 if [ $# -ne 2 ];then
     echo "error: 引数の数が2個ではありません。以下のような正規表現になるように実行してください" >&2
@@ -21,8 +22,8 @@ fi
 # }}}エラー処理
 video_image="`dirname $0`/logo_soundonly.png"
 
-ffmpeg -r 1 -y \
-    -f alsa -i hw:0 -ar 44100 -strict 2 \
+ffmpeg -y \
+    -f image2 -loop 1 -s 512x384 -i "${video_image}" -r 1 \
+    -f alsa -i hw:0 -ar 44100 -strict -2 \
     -f flv "$1/$2 flashver=FMLE/3.0\20(compatible;\20FMSc/1.0) swfUrl=$2"
-#    -f image2 -loop 1 -t 00:00:05 -i "${video_image}" -vcodec libx264 -s 512x384 -b:v 100k \
 # vim:set foldmethod=marker:
